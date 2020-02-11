8cf935a8de7867f02afe6cefa20fec487cedb32c
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-11 19:45:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(txt){
   if txt.upcase{
-    return ""
+    return "NO, NOT SINCE 1938!"
+  }else{
+    
   }
 }
 # Whatever you say to grandma, she should respond with
